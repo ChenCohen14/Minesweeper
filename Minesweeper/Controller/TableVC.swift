971c86501fa070bd46739f5af616da30b76278ca
Chen Cohen <chencohen@Chens-iMac.local>
2019-04-14 12:12:04 -0700

    112

---
+++ Minesweeper/Controller/TableVC.swift
@@ -0,0 +1,45 @@
+//
+//  TableVC.swift
+//  Minesweeper
+//
+//  Created by Chen Cohen on 14/04/2019.
+//  Copyright © 2019 Chen Cohen. All rights reserved.
+//
+
+import UIKit
+
+class TableVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var people = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+extension TableVC : UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.text = ""
+        cell.detailTextLabel?.text = ""
+        return cell
+        
+    }
+    
+    
+}
