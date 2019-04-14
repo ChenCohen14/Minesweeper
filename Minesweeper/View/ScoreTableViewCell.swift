ae05620b7e90862e16857c86ad9f3b5fcf2d700b
Chen Cohen <chencohen@Chens-iMac.local>
2019-04-14 15:32:41 -0700

    work

---
+++ Minesweeper/View/ScoreTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreTableViewCell.swift
+//  Minesweeper
+//
+//  Created by Chen Cohen on 14/04/2019.
+//  Copyright © 2019 Chen Cohen. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+class ScoreTableViewCell:UITableViewCell  {
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+}
